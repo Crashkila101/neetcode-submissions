4e644530b5b70be832abddad1111d7ac4fd2e8a3
Crashkila101 <concoolmccarthy@gmail.com>
2026-08-05 15:10:20 +0100

    Bulk sync: 11 submissions

---
+++ Data Structures & Algorithms/is-anagram/submission-4.py
@@ -0,0 +1,26 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+
+        # countS, countT = {}, {}
+
+        # for i in range(len(s)):
+        #     countS[s[i]] = countS.get(s[i], 0) + 1
+        #     countT[t[i]] = countT.get(t[i], 0) + 1
+        # for c in countS:
+        #     if countS[c] != countT.get(c, 0):
+        #         return False
+        # return True
+
+        counter = {}
+
+        for char in s:
+            counter[char] = counter.get(char, 0) + 1
+
+        for char in t:
+            if char not in counter or counter[char] == 0:
+                return False
+            counter[char] -= 1
+        
+        return True
---
+++ Data Structures & Algorithms/two-integer-sum/submission-3.py
@@ -0,0 +1,9 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        found = {}
+        for i, num in enumerate(nums):
+            complement = target - num
+            if complement in found:
+                return [found[complement], i]
+            found[num] = i
+        
